9611f25fcf7872f7dd26f2b0bd568e7f37004242
Rocio Keuro <rociokeuro@MacBook-Pro-de-Rocio.local>
2022-12-06 16:32:29 +0100

    mk/ajoutDalEleve

---
+++ PitAgora/Models/DalEleve.cs
@@ -0,0 +1,44 @@
+﻿using PitAgora.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
+
+namespace PitAgora
+{
+    public class DalEleve : IDisposable
+    {
+        private BddContext _bddContext;
+        public DalEleve()
+        {
+            _bddContext = new BddContext();
+        }
+
+      
+
+        public List<Eleve> ObtientTousLesELeves()
+        {
+            return _bddContext.Eleves.ToList();
+        }
+       
+
+        public int CreerEleve(string nom, string prenom, string mail, string motDePasse, string adresse, int creditCours)
+        {
+            Dal dal = new Dal(); 
+            int personneId = dal.CreerPersonne(nom, prenom);
+            int utilisateurId = dal.CreerUtilisateur(personneId, mail, motDePasse, adresse);
+            Eleve eleve = new Eleve() { UtilisateurId = utilisateurId, CreditCours = creditCours };
+            _bddContext.Eleves.Add(eleve);
+            _bddContext.SaveChanges(); /*A ne pas oublier, enregistre la modif*/
+            return eleve.Id;
+
+        }
+
+        public void Dispose()
+        {
+            _bddContext.Dispose();
+        }
+
+    }
+}
