75d08e1df852a0a7002a1fba3582eff812002749
Jerrylaevis <jerome.vallin@laposte.net>
2022-12-07 14:32:45 +0100

    Ajout EleveController et sa page Index

---
+++ PitAgora/Controllers/EleveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PitAgora.Controllers
+{
+    public class EleveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
