3f8da31f1309750aeb916734e07d98abc9581d72
Francky-san <ftanguy9@yahoo.fr>
2022-12-13 15:19:16 +0100

    Ajout méthode AfficherInfos HomeController

--- PitAgora/Controllers/HomeController.cs
+++ PitAgora/Controllers/HomeController.cs
@@ -15,5 +15,27 @@ namespace PitAgora.Controllers
             return View();
         }
 
+        public IActionResult AfficherInfosPerso(int Id)
+        {
+            DalEleve dal = new DalEleve();
+            Eleve eleve = dal.ObtientTousLesELeves().Where(e => e.UtilisateurId == Id).FirstOrDefault();
+            DalProf dalProf = new DalProf();
+            Professeur prof = dalProf.ObtientTousLesProfesseurs().Where(e => e.UtilisateurId == Id).FirstOrDefault();
+            DalParent dalParent= new DalParent();
+            Eleve eleve1 = dal.ObtientTousLesELeves().Where(e=>e.ParentId == Id).FirstOrDefault();
+            if (eleve != null)
+            {
+                return View("AccueilEleve", eleve);
+            }
+            else if (prof != null)
+            {
+                return View("AccueilProfesseur", prof);
+            }
+            else if(eleve1 != null)
+            {
+                return View("AccueilParent", eleve1);
+            }
+            return View();
+        }
     }
 }
