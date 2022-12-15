9673a4dcf38942b72c72362119ee281b0eade02b
Mila KEURO <keuromila@gmail.com>
2022-12-15 16:02:09 +0100

    mk/MilaFront

--- PitAgora/Models/BddContext.cs
+++ PitAgora/Models/BddContext.cs
@@ -25,7 +25,7 @@ namespace PitAgora.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("server = localhost; user id = root; password = Hina ; database = PitAgora");
+            optionsBuilder.UseMySql("server = localhost; user id = root; password = Triqui24+ ; database = PitAgora");
         }
 
         //Methode suivante relatives à authentification et autorisation//////////////////////////////////////////////////
